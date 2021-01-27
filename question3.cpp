5c044545d27ce40fa7536653c6390bd2ec245139
nueltogo <nunana.togo@turntabl.io>
2021-01-27 10:56:28 +0000

    Finished third question

--- question3.cpp
+++ question3.cpp
@@ -7,30 +7,59 @@ void output(int i)
 
 void output(char i)
 {
-	        std::cout << "This is a char: " << i << std::endl;
+        std::cout << "This is a char: " << i << std::endl;
 }   
 
 void output(float i)
 {
-	        std::cout << "This is a float: " << i << std::endl;
+        std::cout << "This is a float: " << i << std::endl;
 }   
 
 void output(long i)
 {
-	        std::cout << "This is a long: " << i << std::endl;
+	std::cout << "This is a long: " << i << std::endl;
 }   
 
-void output(char [] i)
+void output(char * i)
 {
-	        std::cout << "This is an string: " << i << ": ";
-		
+       std::cout << "This is an string: " << i << ": ";
+       while(*i != '\0')
+       {
+	       std::cout << *i;
+	       i++;
+       }
+       std::cout << std::endl;
 }   
 
+void output(char * i,int count)
+{
+       std::cout << "This is an string : " << i << ": ";
+       for(int h=0; h < count; h++ )
+       {
+	       std::cout << *i;
+	       i++;
+       }
+       std::cout << std::endl;
+}
+
+void output(char * i,int count, int last)
+{
+       std::cout << "This is an string: " << i << ": ";
+       for(int h=count; h < count+last; h++)
+       {
+	       std::cout << *i;
+	       i++;
+       }
+       std::cout << std::endl;
+}
 int main()
 {
-	long myname = 45896;
+	char myname[] {"Nunana"};
+	char * mynameptr {myname};
 
 	output(378854);
-	output("hi there");
+//	output("hi there");
 	output(myname);
+	output(myname,3);
+	output(myname,3,4);
 }

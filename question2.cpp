9af661d05fcaa73195dd94c2e411b48b048a6469
nueltogo <nunana.togo@turntabl.io>
2021-01-27 09:22:35 +0000

    Finished with second question

---
+++ question2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+void output(int i)
+{
+	std::cout << "This is an int: " << i << std::endl;
+}
+
+void output(char i)
+{
+	        std::cout << "This is a char: " << i << std::endl;
+}   
+
+void output(float i)
+{
+	        std::cout << "This is a float: " << i << std::endl;
+}   
+
+void output(long i)
+{
+	        std::cout << "This is a long: " << i << std::endl;
+}   
+
+void output(std::string i)
+{
+	        std::cout << "This is an string: " << i << std::endl;
+}   
+
+int main()
+{
+	long myname = 45896;
+
+	output(378854);
+	output("hi there");
+	output(myname);
+}
